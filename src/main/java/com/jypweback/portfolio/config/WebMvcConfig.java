fabe9e83851cbf9ae643e18be4f6568dd3799483
jypweback <jypweback@hunet.co.kr>
2019-10-20 20:12:32 +0900

    500 error

--- src/main/java/com/jypweback/portfolio/config/WebMvcConfig.java
+++ src/main/java/com/jypweback/portfolio/config/WebMvcConfig.java
@@ -14,7 +14,7 @@ import javax.persistence.Converter;
  * Github : http://github.com/jypweback
  */
 
-/*
+
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer, ApplicationContextAware {
@@ -32,13 +32,12 @@ public class WebMvcConfig implements WebMvcConfigurer, ApplicationContextAware {
         // TODO Auto-generated method stub
         registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
         registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/");
-        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/").setCachePeriod(0);
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
         registry.addResourceHandler("/scss/**").addResourceLocations("classpath:/static/scss/");
-        registry.addResourceHandler("/vendor/**").addResourceLocations("classpath:/static/vendor/").setCachePeriod(0);
+        registry.addResourceHandler("/vendor/**").addResourceLocations("classpath:/static/vendor/");
         registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/resources/");
     }
 
 
 
 }
-  */
